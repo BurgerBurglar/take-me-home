506c2d91d185b69d9f61e55d607cbad83c22e7db
Shuo Tian <tianshuo1996@outlook.com>
2022-02-09 07:09:52 -0600

    animal card link

--- components/AnimalCard.tsx
+++ components/AnimalCard.tsx
@@ -1,4 +1,15 @@
-import { Box, Flex, Heading, Stack, Tag, Text, Wrap } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Heading,
+  LinkBox,
+  LinkOverlay,
+  Stack,
+  Tag,
+  Text,
+  Wrap,
+} from "@chakra-ui/react";
+import NextLink from "next/link";
 import React from "react";
 import { Animal } from "../types/animals";
 import defaultPic from "../utils/defaultPic";
@@ -13,13 +24,20 @@ const AnimalCard: React.FC<AnimalCardProps> = ({ animal }) => {
   const topTags = animal.tags.slice(0, 3);
   return (
     <>
-      <Flex
-        direction="column"
+      <LinkBox
+        display="flex"
+        flexDir="column"
         border="1px"
         borderColor="gray.200"
         rounded="md"
         overflow="hidden"
         shadow="md"
+        _hover={{
+          bgColor: "purple.50",
+          shadow: "lg",
+          transform: "translateY(-0.1rem)",
+          transition: "transform 0.2s ease-in",
+        }}
       >
         <NextImage
           src={animal.primary_photo_cropped?.small ?? defaultPic(animal)}
@@ -29,7 +47,9 @@ const AnimalCard: React.FC<AnimalCardProps> = ({ animal }) => {
         />
         <Stack direction="column" spacing={2} justify="center" p={5} pt={3}>
           <Heading as="h3" fontSize="1.8rem" color="purple.700">
-            {animal.name}
+            <NextLink href={`/animal/${animal.id}`} passHref>
+              <LinkOverlay>{animal.name}</LinkOverlay>
+            </NextLink>
             {animal.distance === null ? null : (
               <Box
                 as="span"
@@ -51,7 +71,7 @@ const AnimalCard: React.FC<AnimalCardProps> = ({ animal }) => {
             ))}
           </Wrap>
         </Stack>
-      </Flex>
+      </LinkBox>
     </>
   );
 };
